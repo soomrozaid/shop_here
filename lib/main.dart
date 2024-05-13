b41e9b76338ae7c3646c6ae0159d80d520fce74c
soomrozaid <soomrozaid@gmail.com>
2024-05-13 00:12:29 -0600

    notification figured out

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,54 @@
+import 'dart:convert';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:shop_here/firebase_options.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  initFirebaseMessaging();
+  FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundMessaging);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    if (message.notification != null) {
+      print("Notification onMessage: ${message.notification.toString()}");
+    }
+  });
   runApp(const MyApp());
+}
+
+Future _firebaseBackgroundMessaging(RemoteMessage message) async {
+  if (message.notification != null) {
+    print("You got message: ${message.notification}");
+  }
+}
+
+Future<void> initFirebaseMessaging() async {
+  FirebaseMessaging instance = FirebaseMessaging.instance;
+  // NotificationSettings firebaseSetttings =
+  //     await instance.requestPermission(provisional: true);
+
+  NotificationSettings settings = await instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  String? apnsToken = await instance.getAPNSToken();
+  String? token = await instance.getToken();
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  print(apnsToken);
+  print(token);
 }
 
 class MyApp extends StatelessWidget {
@@ -81,7 +127,10 @@ class _MyHomePageState extends State<MyHomePage> {
     Position position = await Geolocator.getCurrentPosition();
     // LocationAccuracyStatus location = await Geolocator.getLocationAccuracy();
 
-    setState(() => positionInfo = position.toString());
+    double distanceToWalmart = Geolocator.distanceBetween(position.latitude,
+        position.longitude, 52.15292066644239, -106.62147190964006) / 1000;
+
+    setState(() => positionInfo = distanceToWalmart.toString());
     // if (permission == LocationPermission.denied) {
     //   // permission = await Geolocator.requestPermission();
     //   // Geolocator.openLocationSettings();
@@ -101,6 +150,49 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // print(permission);
     // return "";
+  }
+
+  Future<void> sendNotification() async {
+    // http.post(Uri.parse("https://fcm.googleapis.com/fcm/send"), headers: {
+    //   "Authorization":
+    //       "Bearer AAAAr7jOjek:APA91bFd_wP6S7ANGKNNZlzsZYVa65ZYApVPKv3NmcUQCryUDPkYVrvb1XnaNQ7CvII_KyPGqKRxS_bHYtdma666GdNfGW7bdo1qijo1cAE3HuLyUMnnLuDFkS3aw3oTU3_rVvqPqEQL"
+    // }, body: {
+    //   "to":
+    //       "dYLSCM9i6kuAun3vq-wfy1:APA91bGVa-FrJ67FlHiiFjVrLKtjIMXLNR-7EOnHb8bInWa0raFl6x9pHjZ4bKsD1vQL9ultWBSU5cYzntsJpDco3FX9BFL-N4u2xdL8SE6E1eZHZgYMJzXw3wJsUpUsMuPmSN9KEgtd",
+    //   "notification": {
+    //     "title": "Hello from the APIs",
+    //     "body": "I must have called a thousand times.",
+    //     "sound": "default"
+    //   }
+    // });
+    String url = 'https://fcm.googleapis.com/fcm/send';
+    String token =
+        "AAAAr7jOjek:APA91bFd_wP6S7ANGKNNZlzsZYVa65ZYApVPKv3NmcUQCryUDPkYVrvb1XnaNQ7CvII_KyPGqKRxS_bHYtdma666GdNfGW7bdo1qijo1cAE3HuLyUMnnLuDFkS3aw3oTU3_rVvqPqEQL";
+    final response = await http.post(Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({
+          "to":
+              "dYLSCM9i6kuAun3vq-wfy1:APA91bGVa-FrJ67FlHiiFjVrLKtjIMXLNR-7EOnHb8bInWa0raFl6x9pHjZ4bKsD1vQL9ultWBSU5cYzntsJpDco3FX9BFL-N4u2xdL8SE6E1eZHZgYMJzXw3wJsUpUsMuPmSN9KEgtd",
+          "notification": {
+            "title": "Hello from the APIs",
+            "body": "I must have called a thousand times.",
+            "sound": "default"
+          }
+        }));
+    print('Token : ${token}');
+    print(response);
+
+    if (response.statusCode == 200) {
+      Map formattedResponse = jsonDecode(response.body);
+      print(formattedResponse);
+      // return themes;
+    } else {
+      throw Exception('Failed to load themes');
+    }
   }
 
   @override
@@ -146,6 +238,40 @@ class _MyHomePageState extends State<MyHomePage> {
             //       print(snapshot);
             //       return Text(snapshot.toString());
             //     }),
+            StreamBuilder(
+              stream: FirebaseMessaging.onMessage,
+              builder: (BuildContext context,
+                  AsyncSnapshot<RemoteMessage> snapshot) {
+                if (snapshot.hasData && snapshot.data?.notification != null) {
+                  return ColoredBox(
+                    color: Colors.blueGrey,
+                    child: Column(
+                      children: [
+                        Text(
+                          snapshot.data!.notification?.title ??
+                              "No title provided",
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 24),
+                        ),
+                        Text(
+                          snapshot.data!.notification?.body ??
+                              "no body provided",
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return const ColoredBox(
+                  color: Colors.redAccent,
+                  child: Text(
+                    "No New Notifications",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              },
+            ),
             Text(positionInfo),
             const Text(
               'You have pushed the button this many times:',
@@ -156,8 +282,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton.icon(
                 onPressed: getUserLocation,
-                icon: const Icon(Icons.location_city),
-                label: const Text("Get Location"))
+                icon: const Icon(Icons.location_searching),
+                label: const Text("Get Location")),
+            ElevatedButton.icon(
+                onPressed: sendNotification,
+                icon: const Icon(Icons.notifications),
+                label: const Text("Send Notification")),
           ],
         ),
       ),
